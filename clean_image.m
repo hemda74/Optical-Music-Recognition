39ef91df07e93b1fbf17f8495e9517d815af0c22
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2023-01-16 03:44:05 +0200

    Update clean_image step

--- clean_image.m
+++ clean_image.m
@@ -2,7 +2,6 @@ function [ subimg_clean ] = clean_image( subimg_no_sl, n )
 % CLEAN IMAGE 
 %   Inputs, subimages without staff lines and number of subimages
 %   Outputs, cleaned subimages
-
     % Structural element similar to the holes created from the removal of
     % the staff lines
     se_line = strel('line', 4, 90);
